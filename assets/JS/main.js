9ddd5f4c44403ba7fdf802e7edc3278732c14a29
Siddharth Tayde <taydesiddharth999@gmail.com>
2026-06-16 22:36:43 +0530

    Add type-writer effect for hero section title

---
+++ assets/JS/main.js
@@ -0,0 +1,26 @@
+// ============================== SECTION TITLE TYPE WRITER EFFECT ===============================
+const title = document.querySelector('.section_title');
+
+const textArray = [
+    `Search for 'Mountains'`,
+    `Search for 'Gym'`,
+    `Search for 'Nature'`,
+    `Search for 'Sun-set'`,
+    `Search for 'Aesthetics'`,
+    `Search for 'Cats'`
+]
+
+let i = 0;
+const setKeyword = () => {
+    title.textContent = textArray[i];
+        title.classList.add('my_element')
+        i++;
+        if (i === textArray.length) {
+            i = 0;
+            return;
+        };
+};
+
+setInterval(() => {
+    setKeyword();
+}, 5000);
